c6c6b14c2f997da564ceebc93bb7d651a807450c
yanl.fu <yanl.fu@outlook.com>
2019-03-23 23:36:42 +0800

    配置中心

---
+++ center-config/src/main/java/com/ikinloop/centerconfig/CenterConfigApplication.java
@@ -0,0 +1,15 @@
+package com.ikinloop.centerconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class CenterConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CenterConfigApplication.class, args);
+    }
+
+}
---
+++ center-config/src/test/java/com/ikinloop/centerconfig/CenterConfigApplicationTests.java
@@ -0,0 +1,18 @@
+/*
+package com.ikinloop.centerconfig;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CenterConfigApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+*/
